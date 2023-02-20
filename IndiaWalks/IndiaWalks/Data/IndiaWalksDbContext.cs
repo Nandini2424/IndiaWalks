ba499d93246fba7e5ee7c3958988faf785b706e7
nandini.tomar <nandini.tomar@globallogic.com>
2023-02-20 15:47:07 +0530

    Second Database Done

---
+++ IndiaWalks/IndiaWalks/Data/IndiaWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using IndiaWalks.Models;
+using IndiaWalks.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndiaWalks.Data
+{
+    public class IndiaWalksDbContext:DbContext
+    {
+        //Contructor 
+        public IndiaWalksDbContext(DbContextOptions<IndiaWalksDbContext> option) :base(option){ }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> walks { get; set; }
+        public DbSet<WalkDifficulty> walkDifficulty { get; set; }
+
+    }
+}
---
+++ IndiaWalks/IndiaWalks/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿using IndiaWalks.Models.Domain;
+
+namespace IndiaWalks.Models
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string  Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        public Region Regions { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
